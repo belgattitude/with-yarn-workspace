79bc75b6891bf9e776e3454cac05b590aa9d1cb3
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-08-10 11:16:09 +0200

    move to typescript

--- packages/web-app/pages/index.tsx
+++ packages/web-app/pages/index.tsx
@@ -1,11 +1,11 @@
-import foo from '@package/foo'
+import {fooData} from '@package/foo'
 import Bar from '@package/bar'
 
 export default function Home() {
   return (
     <div>
       Imported modules from another workspace:
-      <pre>{foo}</pre>
+      <pre>{fooData.message}</pre>
       <Bar />
     </div>
   )
